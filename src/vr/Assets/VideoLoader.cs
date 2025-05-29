346e5231d73c17e84b6efa94ce03f1f66fbccdb5
David Herren <info@davidherren.ch>
2025-05-29 15:45:41 +0200

    Update VideoLoader.cs

--- src/vr/Assets/VideoLoader.cs
+++ src/vr/Assets/VideoLoader.cs
@@ -4,28 +4,30 @@ using System.IO;
 
 public class VideoLoader : MonoBehaviour
 {
-    public VideoPlayer videoPlayer;
+  public VideoPlayer videoPlayer;
+  public string videoFileName = "skies.mp4";
 
-    void Start()
-    {
-        string videoPath = Path.Combine(Application.streamingAssetsPath, "skies.mp4");
+  void Start()
+  {
+    string videoPath = Path.Combine(Application.streamingAssetsPath, videoFileName);
 
 #if UNITY_ANDROID && !UNITY_EDITOR
         // On Android, Application.streamingAssetsPath is already a valid URL
         videoPlayer.url = videoPath;
 #else
-        // In the Editor (and on other platforms), prepend file:// to the path
-        videoPlayer.url = "file://" + videoPath;
+    // In the Editor (and on other platforms), prepend file:// to the path
+    videoPlayer.url = "file://" + videoPath;
 #endif
 
-        videoPlayer.source = VideoSource.Url;
-        videoPlayer.isLooping = true;
-        videoPlayer.prepareCompleted += vp => {
-            Debug.Log("Video is prepared. Starting playback...");
-            videoPlayer.Play();
-        };
+    videoPlayer.source = VideoSource.Url;
+    videoPlayer.isLooping = true;
+    videoPlayer.prepareCompleted += vp =>
+    {
+      Debug.Log("Video is prepared. Starting playback...");
+      videoPlayer.Play();
+    };
 
-        Debug.Log("Loading video from: " + videoPlayer.url);
-        videoPlayer.Prepare();
-    }
+    Debug.Log("Loading video from: " + videoPlayer.url);
+    videoPlayer.Prepare();
+  }
 }
